142b8698a459ee3aadc94423677ff98a05d13729
chibao270199 <nguyenchibao27011999@gmail.com>
2021-07-06 11:06:49 +0700

    Tính C và S Hình Tròn

---
+++ bai5.py
@@ -0,0 +1,11 @@
+class Hinh_Tron:
+    def __init__(self,ban_kinh):
+        self.r=ban_kinh
+    def chu_vi(seft):
+        return seft.r*2*3.14
+    def dien_tich(seft):
+        return seft.r*seft.r*3.14
+r=int(input("Nhập Bán Kính Hình Tròn: "))
+a = Hinh_Tron(r)
+print("Chu Vi Hình Tròn là: ",a.chu_vi())
+print("Diện Tích Hình Tròn là: ",a.dien_tich())
