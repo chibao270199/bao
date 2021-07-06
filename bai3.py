e8d0dbd1d2d792efbb13c5ffe5d41b0cc90218bb
chibao270199 <nguyenchibao27011999@gmail.com>
2021-07-06 10:04:02 +0700

    6/7/2021

---
+++ bai3.py
@@ -0,0 +1,8 @@
+class sieunhan:
+    def __init__(self,ten,mau,vukhi):
+        self.ten = ten
+        self.mau = mau
+        self.vukhi =vukhi
+sieu_nhan_a=sieunhan("bao","do","kiem")
+print(sieu_nhan_a.ten)
+print(sieu_nhan_a.mau)
