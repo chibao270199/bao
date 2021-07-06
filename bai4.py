d2368b492406c96bde4a5a80b94deaa37b222797
chibao270199 <nguyenchibao27011999@gmail.com>
2021-07-06 15:54:02 +0700

    7/6/2021

--- bai4.py
+++ bai4.py
@@ -6,11 +6,20 @@ class Hinh_vuong:
         self.rong = chieu_rong
     #tính chu vi hình vuông
     def chu_vi(seft):
-        return (seft.dai+seft.rong)*2
+       return (seft.dai+seft.rong)*2
     def dien_tich(seft):
         return seft.rong*seft.dai
+    @classmethod
+    def cap_nhat(cls,s):
+        lst=s.split('-')
+        new_lst=[st.strip() for st in lst]
+        dai,rong = new_lst
+        return cls("(-.-)","Hehe")
 a=(int(input("Hãy Nhập Chiều Dài: ")))
 b=(int(input("Hãy Nhập Chiều Rộng: ")))
 c=Hinh_vuong(a,b)
-print("Chu Vi Hình Chử Nhật là: ",c.chu_vi())
+print("Chu Vi Hình Chữ Nhật là: ",c.chu_vi())
 print("Diện Tịch Hình Chử Nhật là: ",c.dien_tich())
+infor_str= "The - End"
+d= Hinh_vuong.cap_nhat(infor_str)
+print(d.__dict__)
